d3420a9639381adc64903eae075cd09dfeec6f94
Oscar Castillo <goldoac@live.com>
2022-11-23 14:19:31 -0600

    initial project

---
+++ Photo HQ/Photo HQ/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Photo HQ
+//
+//  Created by Oscar Castillo on 11/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Photo HQ/Photo HQ/Photo_HQApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_HQApp.swift
+//  Photo HQ
+//
+//  Created by Oscar Castillo on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Photo_HQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
