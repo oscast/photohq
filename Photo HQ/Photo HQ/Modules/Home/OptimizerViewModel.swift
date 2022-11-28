065510ffa7c5fae8c56f5f3147aa21135f386f3d
Oscar Castillo <goldoac@live.com>
2022-11-28 14:16:10 -0600

    resizes image after optimization.

--- Photo HQ/Photo HQ/Modules/Home/OptimizerViewModel.swift
+++ Photo HQ/Photo HQ/Modules/Home/OptimizerViewModel.swift
@@ -9,6 +9,11 @@ import UIKit
 import Vision
 import ImageIO
 
+enum RatioType {
+    case width
+    case height
+}
+
 class OptimizerViewModel: ObservableObject {
     @Published var convertedImage: UIImage?
     @Published var isOptimizing: Bool = false
@@ -16,6 +21,10 @@ class OptimizerViewModel: ObservableObject {
     
     var imageCropAndScaleOption: VNImageCropAndScaleOption = .scaleFill
     let configuration = MLModelConfiguration()
+    
+    var originalImageRatio: CGSize = .zero
+    
+    var imageRatio: [RatioType: CGFloat] = [:]
     
     lazy var visionRequest: VNCoreMLRequest = {
         do {
@@ -34,7 +43,7 @@ class OptimizerViewModel: ObservableObject {
                         let ciContext = CIContext()
                         guard let safeCGImage = ciContext.createCGImage(ciImage, from: ciImage.extent) else { return }
                         let resultImage = UIImage(cgImage: safeCGImage)
-                        self.convertedImage = resultImage
+                        self.resizeImageProportions(image: resultImage)
                         self.isOptimizing = false
                     } else {
                         self.isOptimizing = false
@@ -51,7 +60,7 @@ class OptimizerViewModel: ObservableObject {
     
     func transformImage(image: UIImage) {
         guard let ciImage = CIImage(image: image) else { return }
-        
+        setImageProportions(for: image)
         isOptimizing = true
         let orientation = CGImagePropertyOrientation(image.imageOrientation)
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
@@ -65,6 +74,62 @@ class OptimizerViewModel: ObservableObject {
             }
         }
     }
+    
+    func resizeImageProportions(image: UIImage) {
+        guard let ratio = Array(imageRatio.keys).first, let value = imageRatio[ratio] else { return }
+        switch ratio {
+        case .width:
+            let width = image.size.width
+            let targetWidth = width * value
+            convertedImage = resizeImage(image: image, targetSize: CGSize(width: targetWidth, height: image.size.height))
+        case .height:
+            let height = image.size.height
+            let targetHeight = height * value
+            convertedImage = resizeImage(image: image, targetSize: CGSize(width: image.size.width, height: targetHeight))
+        }
+    }
+    
+    func setImageProportions(for image: UIImage) {
+        if image.size.width > image.size.height {
+            let ratio = image.size.height / image.size.width
+            imageRatio = [.height: ratio]
+        }
+        
+        if image.size.height > image.size.width {
+            let ratio = image.size.width / image.size.height
+            imageRatio = [.width: ratio]
+        }
+        
+        if image.size.width == image.size.height {
+            imageRatio = [ : ]
+        }
+    }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+       let size = image.size
+       
+       let widthRatio  = targetSize.width  / size.width
+       let heightRatio = targetSize.height / size.height
+       
+       // Figure out what our orientation is, and use that to form the rectangle
+       var newSize: CGSize
+       if(widthRatio > heightRatio) {
+           newSize = CGSize(width: size.width * widthRatio, height: size.height * heightRatio)
+       } else {
+           newSize = CGSize(width: size.width * heightRatio,  height: size.height * widthRatio)
+       }
+       
+       // This is the rect that we've calculated out and this is what is actually used below
+       let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+       
+       // Actually do the resizing to the rect using the ImageContext stuff
+       UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+       image.draw(in: rect)
+       let newImage = UIGraphicsGetImageFromCurrentImageContext()
+       UIGraphicsEndImageContext()
+       
+       return newImage!
+   }
 }
 
 extension CGImagePropertyOrientation {
